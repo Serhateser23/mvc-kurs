80f710ee3e2457b73ec652da56ba4a3b0ff168a7
Serhat <sees23@student.bth.se>
2024-04-17 15:50:22 +0200

    Adding new files, problem Vs code2

--- src/Controller/DiceGamecontroller.php
+++ src/Controller/DiceGamecontroller.php
@@ -8,7 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DiceGameController extends AbstractController
+class DiceGamecontroller extends AbstractController
 {
     #[Route("/game/pig", name: "pig_start")]
     public function home(): Response
